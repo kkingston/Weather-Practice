6841a6575a5976570c13068cabe3b8ca185a6085
kayla <kkingston09@gmail.com>
2021-04-16 00:38:41 +0100

    inital setup of async/fetch with weather api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css'
 import './App.css';
+import "weather-icons/css/weather-icons.css"
+import Weather from './components/weatherMain'
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+//api.openweathermap.org/data/2.5/weather?q=London,uk&appid={API key}
+const API_key= '9c96e135f012980c60e88455c2a55a76'
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {}
+    this.getWeather()
+  };
+
+  getWeather = async () => {
+    // const settings = {
+    //   method: 'GET',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //     'Accept': 'application/json'
+    //   }
+    // }
+    try {
+      const api_call = await fetch(`api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_key}`)
+      const response = await api_call.json();
+      console.log(response)
+    } catch(e) {
+      console.log('error', e)
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Weather/>
+      </div>
+    );
+  };
+};
 
 export default App;
